5b076536b50897b8b9ee7e2e9a1a1fa1c5ebaa9e
Christine Zhou <cazhou@berkeley.edu>
2019-07-10 10:18:30 -0700

    Release proj2.

---
+++ proj2/gitlet/UnitTest.java
@@ -0,0 +1,17 @@
+package gitlet;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/* The suite of all JUnit tests for the gitlet package.
+   @author
+ */
+public class UnitTest {
+
+    @Test
+    public void placeholderTest() {
+    }
+
+}
+
+
